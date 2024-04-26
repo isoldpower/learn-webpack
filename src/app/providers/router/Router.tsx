86e042ab041e27acd8c3ab9d4a271ace2f2c1e5f
isoldpower <Lapin_NR@icloud.com>
2024-04-26 17:53:54 +0500

    feat: welcome page ui

--- src/app/providers/router/Router.tsx
+++ src/app/providers/router/Router.tsx
@@ -1,16 +1,29 @@
 import {createBrowserRouter} from "react-router-dom";
 import {Suspense} from "react";
 import {App} from "@/app/App";
+import {WelcomeLazy} from "@/pages/landing";
+
+const defaultFallback = <div>Loading...</div>
 
 const routes = [
     {
         path: '/',
         element:
-            <Suspense fallback={'Loading...'}>
+            <Suspense fallback={defaultFallback}>
                 <App/>
-            </Suspense>
-    },
-]
+            </Suspense>,
+        children: [
+            {
+                path: '/',
+                element: (
+                    <Suspense fallback={defaultFallback}>
+                        <WelcomeLazy />
+                    </Suspense>
+                )
+            }
+        ]
+    }
+];
 
 export const router = createBrowserRouter(routes);
 export default routes;
